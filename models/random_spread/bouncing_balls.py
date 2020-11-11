a5df93dd734c77f6130fd7ac2f70a7032c5da572
Michael Saunby <mike@saunby.net>
2020-11-11 10:27:17 +0000

    .

--- models/random_spread/bouncing_balls.py
+++ models/random_spread/bouncing_balls.py
@@ -44,7 +44,12 @@ class Ball:
     def infect(self):
         if self.status == Status.VULNERABLE:
             self.status = Status.INFECTIOUS
+            self.infectious = 130
             self.color = (random.randrange(128,256),0,0)
+
+    def immune(self):
+        self.status = Status.IMMUNE
+        self.color = (0,random.randrange(128,256),0)
 
 
 def make_ball(id):
@@ -65,8 +70,11 @@ def make_ball(id):
     ball.y = random.randrange(ball.size, WINDOW_HEIGHT - ball.size)
 
     # Speed and direction of rectangle
-    ball.change_x = random.randrange(-2, 3)
-    ball.change_y = random.randrange(-2, 3)
+    #ball.change_x = random.randrange(-2, 3)
+    #ball.change_y = random.randrange(-2, 3)
+
+    ball.change_x = random.choice([-2, -0.5, 0.5, 2])
+    ball.change_y = random.choice([-2, -0.5, 0.5, 2])
 
     # Color
     ball.color = (0, 0, random.randrange(128,256))
@@ -76,8 +84,6 @@ def make_ball(id):
 
 class MyGame(arcade.Window):
     """ Main application class. """
-
-
 
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
@@ -89,27 +95,28 @@ class MyGame(arcade.Window):
         for i in range(1,100):
             ball = make_ball(i)
             self.ball_list.append(ball)
+        self.blue_points = [(5+WINDOW_WIDTH,WINDOW_HEIGHT),(5+WINDOW_WIDTH,WINDOW_HEIGHT)]
+        self.red_points = [(5+WINDOW_WIDTH,0),(5+WINDOW_WIDTH,0)]
         
     def on_draw(self):
         """
         Render the screen.
         """
-
         # This command has to happen before we start drawing
         arcade.start_render()
 
         for ball in self.ball_list:
             arcade.draw_circle_filled(ball.x, ball.y, ball.size, ball.color)
 
-        for t in range(len(self.totals)//2):
-           arcade.draw_circle_filled(5+WINDOW_WIDTH + t*0.8, self.totals[t*2]*6, 1, arcade.color.RED)
-           arcade.draw_circle_filled(5+WINDOW_WIDTH + t*0.8, (100-self.totals[t*2])*6, 1, arcade.color.BLUE)
-    
+        
+
+
+        self.red_line_strip.draw()
+        self.blue_line_strip.draw()
 
         # Put the text on the screen.
         output = "Balls: {}".format(len(self.ball_list))
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
-
 
 
     def on_update(self, delta_time):
@@ -121,10 +128,21 @@ class MyGame(arcade.Window):
             if ball.status != Status.VULNERABLE:
                 total += 1
         self.totals.append(total)
+        n = len(self.totals)
+        self.red_points.append((5+WINDOW_WIDTH + n*0.4, (total*6)))
+        self.blue_points.append((5+WINDOW_WIDTH + n*0.4, ((100-total)*6)))
+
+
+        self.red_line_strip = arcade.create_line_strip(self.red_points, arcade.color.RED, 2)
+        self.blue_line_strip = arcade.create_line_strip(self.blue_points, arcade.color.BLUE, 2)
 
         for ball in self.ball_list:
             ball.x += ball.change_x
             ball.y += ball.change_y
+            if ball.status == Status.INFECTIOUS:
+                ball.infectious -= 1
+                if ball.infectious == 0:
+                    ball.immune()
 
             # Collision with walls
             if ball.x < ball.size:
@@ -146,14 +164,13 @@ class MyGame(arcade.Window):
                 if other.hit:
                     continue
                 if (-ball.size < (ball.x - other.x) < ball.size) and (-ball.size < (ball.y - other.y) < ball.size):
-                    #c = ball.color
-                    #ball.color = other.color
-                    #other.color = c
                     (ball.change_x,other.change_x) = (other.change_x,ball.change_x)
                     (ball.change_y,other.change_y) = (other.change_y,ball.change_y)
                     other.hit = True
                     if ball.status == Status.INFECTIOUS:
                         other.infect()
+                    if other.status == Status.INFECTIOUS:
+                        ball.infect()
                     ball.hit = True
                     break
 
@@ -164,7 +181,8 @@ class MyGame(arcade.Window):
         """
         Called whenever the mouse button is clicked.
         """
-        ball = make_ball()
+        ball = make_ball(100)
+        ball.infect()
         self.ball_list.append(ball)
 
 
