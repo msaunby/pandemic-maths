4d395555823048e7a40e62c1d7bf3993223fd802
Michael Saunby <mike@saunby.net>
2020-12-29 11:01:45 +0000

    .

--- models/kivy_template/ballcollisions.py
+++ models/kivy_template/ballcollisions.py
@@ -4,6 +4,7 @@
 from math import cos, sin, pi, sqrt
 from random import random, randint
 from random import choice as randchoice
+from itertools import combinations
 
 from kivy.app import App
 from kivy.clock import Clock
@@ -33,38 +34,21 @@ class PongPaddle(Widget):
             self.can_bounce = True
 
 
-class BaseShape(Widget):
-    '''(internal) Base class for moving with touches or calls.'''
+class RegularShape(Widget):
 
     # shape properties
     name = StringProperty('')
     shape = ObjectProperty()
+    infected = False
 
-    def __init__(self, **kwargs):
-        '''Create a shape with size [100, 100]
-        and give it a label if it's named.
-        '''
-        super(BaseShape, self).__init__(**kwargs)
-        self.size_hint = (1000, 1000)
-
-
-    def on_shape(self, instance, value):
-        print("on_shape")
-
-    def on_pos(self, instance, pos):
-
-        self.shape.pos = [self.x, self.y]
-
-
-class RegularShape(BaseShape):
     '''Starting from center and creating edges around for i.e.:
     regular triangles, squares, regular pentagons, up to "circle".
     '''
 
-    def __init__(self, edges=3, color=None, **kwargs):
+    def __init__(self, **kwargs):
         super(RegularShape, self).__init__(**kwargs)
-        
-        color = color or [1.0,0.0,0.0]
+
+        color = [1.0,0.0,0.0]
 
         with self.canvas:
             Color(rgba=(color[0], color[1], color[2], 0.6))
@@ -75,20 +59,57 @@ class RegularShape(BaseShape):
             self.change_x = randchoice([-3, -2, -1, 0, 1, 2, 3])
             self.change_y = randchoice([-3, -2, -1, 0, 1, 2, 3])
 
+
+    def on_shape(self, instance, value):
+        print("on_shape")
+
+    def on_pos(self, instance, pos):
+
+        self.shape.pos = [self.x, self.y]
+    def infect(self):
+        if self.infected:
+            return
+        self.infected = True
+        with self.canvas:
+            Color(rgba=(0, 0, 1.0, 0.6))
+            self.shape = Ellipse(
+                size=(20,20)
+            )
+
+
 class PongGame(Widget):
 
     def __init__(self, shapes, **kwargs):
         super(PongGame, self).__init__(**kwargs)
+        self.register_event_type('on_collision')
         self.shapes = shapes
+
+    def on_collision(self, pair, *args):
+        '''Dispatched when objects collide, gives back colliding objects
+        as a "pair" argument holding their instances.
+        '''
+        print('Collision {} x {}'.format(pair[0].name, pair[1].name))
+        print('Infected {} x {}'.format(pair[0].infected, pair[1].infected))
+        if pair[1].infected: pair[0].infect()
+        if pair[0].infected: pair[1].infect()
 
     def update_shapes(self,delay):
         for shape in self.shapes:
             shape.x += shape.change_x
             shape.y -= shape.change_y    
+            
+
+        # get all combinations from all available shapes
+        if not hasattr(self, 'combins'):
+            self.combins = list(combinations(self.shapes, 2))
+
 
     def update(self, dt):
         # bounce ball off bottom or top
+        
         for ball in self.shapes:
+            ball.color = [0.0,1.0,0.0]
+
             if ball.y < 0:
                 ball.y = 0
                 ball.change_y *= -1
@@ -102,6 +123,14 @@ class PongGame(Widget):
                 ball.x = 300
                 ball.change_x *= -1
         self.update_shapes(self)
+
+        for com in self.combins:
+            x = (com[0].center_x - com[1].center_x) ** 2
+            y = (com[0].center_y - com[1].center_y) ** 2
+            if sqrt(x + y) <= 5:
+                # dispatch a custom event if the objects collide
+                self.dispatch('on_collision', (com[0], com[1]))
+
                
 
 class Collisions(App):
@@ -110,30 +139,16 @@ class Collisions(App):
 
     def __init__(self, **kwargs):
         super(Collisions, self).__init__(**kwargs)
-        # register an event for collision
-        self.register_event_type('on_collision')
 
-    def on_collision(self, pair, *args):
-        '''Dispatched when objects collide, gives back colliding objects
-        as a "pair" argument holding their instances.
-        '''
-        print('Collision {} x {}'.format(pair[0].name, pair[1].name))
-
-    def update_shapes(self,delay):
-        for shape in self.shapes:
-            shape.x += shape.change_x
-            shape.y -= shape.change_y
-
-    
     def build(self):
-
         # list of shapes
         self.shapes = [
             RegularShape(
-                name='{}'.format(x), edges=10
+                name='{}'.format(x)
             ) for x in range(10)
         ]
-
+        self.shapes[0].infect()    
+        
         scene = PongGame(self.shapes)
 
         # move shapes to some random position
@@ -141,7 +156,7 @@ class Collisions(App):
             shape.pos = [randint(50, i - 50) for i in Window.size]
             scene.add_widget(shape)
 
-        Clock.schedule_interval(scene.update, 1.0 / 60.0)
+        Clock.schedule_interval(scene.update, 1.0 / 20.0)
         return scene
 
 
