c2495577d229c161925e27012f909471e87c7338
Michael Saunby <mike@saunby.net>
2020-12-31 15:06:49 +0000

    .

--- models/kivy_template/ballcollisions.py
+++ models/kivy_template/ballcollisions.py
@@ -8,10 +8,10 @@ from itertools import combinations
 
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.uix.label import Label
+#from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
-from kivy.graphics import (Color, Ellipse)
+#from kivy.graphics import (Color, Ellipse)
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import (StringProperty, ObjectProperty)
 from kivy.properties import (
@@ -21,23 +21,13 @@ from kivy.properties import (
 class Arena(Widget):
 
     def on_pos(self, instance, pos):
-        print("arena on_pos")
         self.pos = pos
 
-class PongPaddle(Widget):
-    score = NumericProperty(0)
-    can_bounce = BooleanProperty(True)
+class Indicator(Widget):
 
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball) and self.can_bounce:
-            vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
-            self.can_bounce = False
-        elif not self.collide_widget(ball) and not self.can_bounce:
-            self.can_bounce = True
+    def __init__(self, color, **kwargs):
+        self.color = color
+        super().__init__(**kwargs)
 
 
 class RegularShape(Widget):
@@ -47,49 +37,41 @@ class RegularShape(Widget):
     shape = ObjectProperty()
     infected = False
 
-    '''Starting from center and creating edges around for i.e.:
-    regular triangles, squares, regular pentagons, up to "circle".
-    '''
 
     def __init__(self, **kwargs):
-        super(RegularShape, self).__init__(**kwargs)
+        super().__init__(**kwargs)
 
-        color = [1.0,0.0,0.0]
+        self.y_indicator = Indicator([1,1,0])
+        self.add_widget(self.y_indicator)
+        self.r_indicator = Indicator([1,0,0])
+        self.r_indicator.opacity = 0.0
+        self.add_widget(self.r_indicator)
 
-        with self.canvas:
-            Color(rgba=(color[0], color[1], color[2], 0.6))
-            self.shape = Ellipse(
-                pos=self.pos,
-                size=(20,20)
-            )
-            self.change_x = randchoice([-3, -2, -1, 0, 1, 2, 3])
-            self.change_y = randchoice([-3, -2, -1, 0, 1, 2, 3])
+        self.change_x = randchoice([-3, -2, -1, 0, 1, 2, 3])
+        self.change_y = randchoice([-3, -2, -1, 0, 1, 2, 3])
 
 
     def on_shape(self, instance, value):
         print("on_shape")
 
     def on_pos(self, instance, pos):
-        self.shape.pos = [self.x, self.y]
+        self.y_indicator.pos  = [self.x, self.y]
+        self.r_indicator.pos  = [self.x, self.y]
 
     def infect(self):
         if self.infected:
             return
         self.infected = True
-        with self.canvas:
-            Color(rgba=(0, 0, 1.0, 0.6))
-            self.shape = Ellipse(
-                size=(20,20)
-            )
+        self.y_indicator.opacity = 0.2
+        self.r_indicator.opacity = 1.0    
 
 
 class PongGame(Widget):
 
     arena = ObjectProperty(None)
-    player1 = ObjectProperty(None)
 
     def __init__(self, shapes, **kwargs):
-        super(PongGame, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.register_event_type('on_collision') 
         self.shapes = shapes
 
@@ -97,25 +79,24 @@ class PongGame(Widget):
         '''Dispatched when objects collide, gives back colliding objects
         as a "pair" argument holding their instances.
         '''
-        print('Collision {} x {}'.format(pair[0].name, pair[1].name))
-        print('Infected {} x {}'.format(pair[0].infected, pair[1].infected))
+        #print('Collision {} x {}'.format(pair[0].name, pair[1].name))
         if pair[1].infected: pair[0].infect()
         if pair[0].infected: pair[1].infect()
 
     def update_shapes(self,delay):
         self.arena.x += 10
+        if self.arena.x > 400:
+            self.arena.x = 0
         for shape in self.shapes:
             shape.x += shape.change_x
-            shape.y -= shape.change_y    
-            
+            shape.y -= shape.change_y             
 
-        # get all combinations from all available shapes
+        # get all combinations, used to check for collisions
         if not hasattr(self, 'combins'):
             self.combins = list(combinations(self.shapes, 2))
 
 
     def update(self, dt):
-        # bounce ball off bottom or top
         
         for ball in self.shapes:
             ball.color = [0.0,1.0,0.0]
@@ -144,25 +125,21 @@ class PongGame(Widget):
                
 
 class Collisions(App):
-    #player1 = ObjectProperty(None)
-    #player2 = ObjectProperty(None)
-   
-    
+  
     def __init__(self, **kwargs):
-        super(Collisions, self).__init__(**kwargs)
+        super().__init__(**kwargs)
 
     def build(self):
-        # list of shapes
         self.shapes = [
             RegularShape(
                 name='{}'.format(x)
-            ) for x in range(10)
+            ) for x in range(30)
         ]
         self.shapes[0].infect()    
         
         scene = PongGame(self.shapes)
 
-        # move shapes to some random position
+        # move to random positions
         for shape in self.shapes:
             shape.pos = [randint(50, i - 50) for i in Window.size]
             scene.add_widget(shape)
